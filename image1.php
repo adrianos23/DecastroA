e42487c44de8cd5a92490d5fb0e4db405475904d
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-08-13 22:04:36 +0800

    Update image1.php

--- image1.php
+++ image1.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-<head><title>Assignmet 05</title>
-</head>
+<head><title>Assignmet 05</title></head>
 <body>
 	<h1><center>DeCastroA Photo Gallery</center></h1>
-	<center><img src="30c.jpg" width="200" height="190"></center>	
+	<center><img src="30c.jpg" width="200" height="190">
+	</center>	
 	<br>
-	<center><a href="home.html">Back to home</a>
-	<a href="image2.html">Next Photo</a></center>
+	<center><a href="home.php">Back to home</a>
+	<a href="image2.php">Next Photo</a></center>
 	</br>
 
 </body>
