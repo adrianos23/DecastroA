437696d128d48ca4952252341e099b85a32eda85
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-08-13 21:58:08 +0800

    Create image3.php

---
+++ image3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head><title>Assignmet 05</title>
+</head>
+<body>
+	<h1><center>DeCastroA Photo Gallery</center></h1>
+	<center><img src="ppyt.jpg" width="200" height="260"></center>	
+	<br>
+	<center><a href="home.html">Back to home</a></center>
+	</br>
+
+</body>
+</html>
