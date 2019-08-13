7dd3349f2706dd663410dd8d194a40247ea64e08
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-08-13 21:53:16 +0800

    Create assignment05.php

---
+++ assignment05.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Assignmet 05</title>
+</head>
+<body>
+<center><h1>DeCastroA Photo Gallery</h1>
+	<h2>List of Photos</h2>
+	<a href="image1.html">Photo 1</a>
+	<br>
+	<a href="image2.html">Photo 2</a>
+	<br>
+	<a href="image3.html">Photo 3</a>
+</center>
+</body>
+</html>
