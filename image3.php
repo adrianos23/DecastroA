3b9cd0a2d7518bfc3115d0ff3758d385e66b91ad
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-08-13 22:07:37 +0800

    Update image3.php

--- image3.php
+++ image3.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head><title>Assignmet 05</title>
-</head>
+<head><title>Assignmet 05</title></head>
 <body>
 	<h1><center>DeCastroA Photo Gallery</center></h1>
 	<center><img src="ppyt.jpg" width="200" height="260"></center>	
 	<br>
-	<center><a href="home.html">Back to home</a></center>
+	<center><a href="home.php">Back to home</a></center>
 	</br>
 
 </body>
