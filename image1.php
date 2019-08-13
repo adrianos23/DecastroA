99f4801ae1271027c646f5716b925fec0290423f
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-08-13 21:55:00 +0800

    Create image1.php

---
+++ image1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head><title>Assignmet 05</title>
+</head>
+<body>
+	<h1><center>DeCastroA Photo Gallery</center></h1>
+	<center><img src="30c.jpg" width="200" height="190"></center>	
+	<br>
+	<center><a href="home.html">Back to home</a>
+	<a href="image2.html">Next Photo</a></center>
+	</br>
+
+</body>
+</html>
