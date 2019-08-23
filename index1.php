ded9b1c589bfc1b26d8a985c3565ff1d88613f70
adrianos23 <53499693+adrianos23@users.noreply.github.com>
2019-08-23 20:16:15 +0800

    Rename index.php to index1.php

---
+++ index1.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head><title>Login Page</title></head>
+<body align="center">
+	<h1><u>Login</u></h1>
+		<div class="container">
+    			<label for="uname"><b>Username</b></label>
+    			<input type="text" placeholder="Enter Username" name="uname" required>
+		<br/>
+		<label for="psw"><b>Password</b></label>
+   			 <input type="password" placeholder="Enter Password" name="psw" required>
+		<br/>
+		<br/>
+		<a href="home.html"><button type="submit">Sign In</button></a>
+		<br/>
+		<br/>
+		<br/>
+		<a href="register.html">Register</a>
+</html>
+</body>
